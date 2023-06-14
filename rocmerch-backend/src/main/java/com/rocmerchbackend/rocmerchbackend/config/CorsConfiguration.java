71091bae454e56a8dc279d79a317f149248cc826
Daniel Lee <danielleemusic98@gmail.com>
2023-06-14 14:41:17 +1000

    explicitly adding access control allow origin header

--- rocmerch-backend/src/main/java/com/rocmerchbackend/rocmerchbackend/config/CorsConfiguration.java
+++ rocmerch-backend/src/main/java/com/rocmerchbackend/rocmerchbackend/config/CorsConfiguration.java
@@ -13,6 +13,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
                 .allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173", "http://127.0.0.1:8080", "https://rocmerch-fullstack-app-production.up.railway.app")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .exposedHeaders("Access-Control-Allow-Origin")
                 .allowCredentials(true);
     }
 }
